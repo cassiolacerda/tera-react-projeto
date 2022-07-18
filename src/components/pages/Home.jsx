c40eb7624d68162eb1aababed0fcc856a5703f8f
cassiolacerda <697508+cassiolacerda@users.noreply.github.com>
2022-07-18 19:58:55 -0300

    Adiciona página Home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,19 +1,46 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import logo from "../../images/logo.svg";
 
 export default function Home() {
+  const navigate = useNavigate();
+
+  const [users, setUsers] = React.useState([]);
+  const [currentUser, setCurrentUser] = React.useState("");
+
+  const onChange = (event) => setCurrentUser(event.target.value);
+  const onConfirm = () => navigate(`/users/${currentUser}`);
+
+  React.useEffect(() => {
+    fetch("https://62c4e487abea8c085a7e022a.mockapi.io/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+      });
+  }, []);
+
   return (
     <div className="home-screen">
       <div className="home-screen__logo">
         <img src={logo} className="responsive" alt="" />
       </div>
-      <select className="home-screen__select-users">
-        <option>User 1</option>
-        <option>User 2</option>
-        <option>User 3</option>
+      <select className="home-screen__select-users" onChange={onChange}>
+        <option value="">Selecionar usuário</option>
+        {users
+          .sort((a, b) => a.fn.localeCompare(b.fn))
+          .map((user) => (
+            <option
+              key={user.id}
+              value={user.id}
+            >{`${user.fn} ${user.ln}`}</option>
+          ))}
       </select>
-      <button className="button-primary">Entrar</button>
+      {currentUser && (
+        <button onClick={onConfirm} className="button-primary">
+          Entrar
+        </button>
+      )}
     </div>
   );
 }
